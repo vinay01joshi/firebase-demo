c905067c9a30817a33670a132e68398ad6a35bc0
Vinay Joshi <vinay01joshi@gmail.com>
2017-08-17 14:31:40 +0530

    Updating Object in firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
   courses$:FirebaseListObservable<any[]>;
   course$;
   author$;
-  constructor(db:AngularFireDatabase){
+  constructor(private db:AngularFireDatabase){
     this.courses$ = db.list('/courses');
     this.course$ = db.object('/courses/1');
     this.author$ = db.object('/authors/1');
@@ -29,5 +29,19 @@ export class AppComponent {
       ]
     })
     course.value = '';
+  }
+
+  update(course){
+    this.db.object('/courses/'+course.$key)
+      //.set(course.$value+' UPDATED');
+      // .set({
+      //   title: course.$value + 'UPDATED',
+      //   price: 15
+      // });
+
+      .update({
+          title: 'New title',
+          isLive: true
+      });
   }
 }
